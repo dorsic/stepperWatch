ff6d9c59e76731e101efc7b4709cc2316594a3fc
Marek Dorsic <dorsic@md-MacBook.local>
2018-08-29 06:59:38 +0200

    hand class

--- stepMotor_clock.py
+++ stepMotor_clock.py
@@ -1,61 +1,76 @@
+
 import time
 import RPi.GPIO as GPIO
 
- class StepperDriver(object):
-    pin1 = None
-    pin2 = None
-    pin3 = None
-    pin4 = None
-    stepCount = 8
-    seq = range(0, self.stepCount)
+class StepperDriver(object):
+    seq = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
+    revolution = 512 # steps
 
-     def __init__(pin1, pin2, pin3, pin4):
-        self.pin1 = pin1
-        self.pin2 = pin2
-        self.pin3 = pin3
-        self.pin4 = pin4
+    def __init__(self, pin1, pin2, pin3, pin4, delay=10.0/1000.0):
+        """Delay is best between 5 and 20 ms"""
+        self.pins = [pin1, pin2, pin3, pin4]
+        self.delay = delay
 
-        self.seq[0] = [0,1,0,0]
-        self.seq[1] = [0,1,0,1]
-        self.seq[2] = [0,0,0,1]
-        self.seq[3] = [1,0,0,1]
-        self.seq[4] = [1,0,0,0]
-        self.seq[5] = [1,0,1,0]
-        self.seq[6] = [0,0,1,0]
-        self.seq[7] = [0,1,1,0]
- 
-        GPIO.setup(pin1, GPIO.OUT)
-        GPIO.setup(pin2, GPIO.OUT)
-        GPIO.setup(pin3, GPIO.OUT)
-        GPIO.setup(pin4, GPIO.OUT)
+        for pin in self.pins:
+	        GPIO.setup(pin, GPIO.OUT)
 
-    def setStep(self, w1, w2, w3, w4):
-        GPIO.output(pin1, w1)
-        GPIO.output(pin2, w2)
-        GPIO.output(pin3, w3)
-        GPIO.output(pin4, w4)
+    def setStep(self, w):
+    	for i in range(4):
+        	GPIO.output(self.pins[i], w[i])
 
-    def forward(self, delay, steps):
+    def forward(self, steps):
         for i in range(steps):
-            for j in range(self.stepCount):
-                self.setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-                time.sleep(delay)
-    
-    def backwards(self, delay, steps):
-        for i in range(steps):
-            for j in reversed(range(self.stepCount)):
-                self.setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-                time.sleep(delay)
+            for j in range(len(self.seq)):
+                self.setStep(self.seq[j])
+                time.sleep(self.delay)
 
- 
+    def backwards(self, steps):
+        for i in range(steps):
+            for j in reversed(range(len(self.seq))):
+                self.setStep(self.seq[j])
+                time.sleep(self.delay)
+
+    def forwardAngle(self, degrees):
+        self.forward(int(self.revolution*degrees/360))
+
+    def backwardsAngle(self, degrees):
+        self.backwards(int(self.revolution*degrees/360))
+
+
+class Hand(StepperDriver):
+
+	def __init__(self, minAngle, maxAngle, speed, positions):
+		""" Speed defines the number of steps needed to move from minAngle to maxAngle.
+			Positions defines how many distinc angles can be set.
+		"""
+		self.minAngle = minAngle
+		self.maxAngle = maxAngle
+		self.speed = speed
+		self.steps = 0
+
+	def step(self):
+		self.steps += 1
+		angle = self.minAngle + self.steps*(self.maxAngle-self.minAngle)/self.speed
+		if (self.steps/self.speed*(self.maxAngle-self.minAngle))%((self.maxAngle-self.minAngle)*self.positions) == 0:
+			# make step
+			self.forwardAngle((self.maxAngle-self.minAngle)/self.positions)
+
+
+
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
     m = StepperDriver(12, 16, 18, 22)
     h = StepperDriver(7, 11, 13, 15)
+    hh = Hand(0, 90, 12*60, 12*4)
+    mh = Hand(0, 270, 60, 60)
+
+    print(m.pins)
+    print(h.pins)
     while True:
-        delay = raw_input("Time Delay (ms)?")
-        steps = raw_input("How many steps forward? ")
-        m.forward(int(delay) / 1000.0, int(steps))
-        steps = raw_input("How many steps backwards? ")
-        m.backwards(int(delay) / 1000.0, int(steps))
+        steps = input("How many steps forward? ")
+        m.forward(int(steps))
+        h.forward(int(steps))
+        steps = input("How many steps backwards? ")
+        m.backwards(int(steps))
+        h.backwards(int(steps))
